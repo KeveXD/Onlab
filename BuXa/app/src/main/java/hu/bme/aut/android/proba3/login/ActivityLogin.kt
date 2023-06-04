cb83ac73ee262d0daf710bf11c87fb011e58610d
KeveXD <100792669+KeveXD@users.noreply.github.com>
2023-06-05 01:58:56 +0200

    kesz

--- BuXa/app/src/main/java/hu/bme/aut/android/proba3/login/ActivityLogin.kt
+++ BuXa/app/src/main/java/hu/bme/aut/android/proba3/login/ActivityLogin.kt
@@ -23,79 +23,81 @@ class ActivityLogin : AppCompatActivity(), LoginListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Az activity_login.xml fájlhoz kötjük a layoutot
         val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModelLogin = ViewModelProvider(this).get(ViewModelLogin::class.java)
-        ViewModelHolder.viewModelLogin = viewModelLogin
-        val registerText: TextView=findViewById(R.id.text_register)
-        val SignInButton: Button =findViewById(R.id.button_sign_in)
 
+        // Létrehozzuk a ViewModelLogin objektumot
+        val viewModelLogin = ViewModelProvider(this).get(ViewModelLogin::class.java)
+
+        // A ViewModelLogin objektumot elmentjük a ViewModelHolder segítségével
+        ViewModelHolder.viewModelLogin = viewModelLogin
+
+        // TextView és Button elemek inicializálása
+        val registerText: TextView = findViewById(R.id.text_register)
+        val signInButton: Button = findViewById(R.id.button_sign_in)
+
+        // A regisztráció szövegére kattintás eseménykezelője
         registerText.setOnClickListener{
+            // Átirányítás az ActivityRegister aktivitásra
             val intent = Intent(this, ActivityRegister::class.java)
             startActivity(intent)
         }
 
+        // A ViewModelLogin objektum beállítása a binding objektumban
+        binding.viewmodel = viewModelLogin
 
+        // A ViewModelLogin objektum beállítása a LoginListener interfésszel
+        viewModelLogin.listener = this
 
-        binding.viewmodel=viewModelLogin
-        viewModelLogin.listener=this
-        auth= Firebase.auth
-        viewModelLogin.auth=auth
-        viewModelLogin.activity=this
+        // Firebase hitelesítés inicializálása
+        auth = Firebase.auth
+        viewModelLogin.auth = auth
+        viewModelLogin.activity = this
 
+        // Az email és jelszó mezők alapértelmezett értékeinek beállítása a ViewModelLogin objektumban
         binding.viewmodel = viewModelLogin.apply {
             email = "proba@gmail.com"
             password = "Proba123"
         }
 
-        viewModelLogin.listener = this
-        auth = Firebase.auth
-        viewModelLogin.auth = auth
-        viewModelLogin.activity = this
-
+        // Az email mező figyelése és értékének beállítása a ViewModelLogin objektumban
         binding.editTextEmail.addTextChangedListener {
             viewModelLogin.email = it.toString()
         }
 
-        binding.editTextPassword.addTextChangedListener {
-            viewModelLogin.password = it.toString()
-        }
-
-
-        binding.editTextEmail.addTextChangedListener {
-            viewModelLogin.email = it.toString()
-        }
-
+        // A jelszó mező figyelése és értékének beállítása a ViewModelLogin objektumban
         binding.editTextPassword.addTextChangedListener {
             viewModelLogin.password = it.toString()
         }
 
     }
 
-    //UI elemekkel valo kommunikalas
+    // Toast üzenet megjelenítése
     override fun toast(szoveg: String) {
         Toast.makeText(this, szoveg, Toast.LENGTH_LONG).show()
     }
 
-
-    override fun check(email: String,password: String) {
+    // Felhasználó bejelentkezésének ellenőrzése
+    override fun check(email: String, password: String) {
+        // Firebase hitelesítés: bejelentkezés az email és jelszó segítségével
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Toast.makeText(baseContext, "Autentikáció sikeres csekkolás", Toast.LENGTH_SHORT).show()
+                    // Ha sikeres volt a bejelentkezés, átirányítás a MenuActivity aktivitásra
+                    Toast.makeText(baseContext, "Autentikáció sikeres belépés", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, MenuActivity::class.java)
                     startActivity(intent)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(baseContext, "Autentikációs hiba csekkolásnál", Toast.LENGTH_SHORT).show()
+                    // Ha sikertelen volt a bejelentkezés, hibaüzenet megjelenítése
+                    Toast.makeText(baseContext, "Autentikációs hiba belépésnél", Toast.LENGTH_SHORT).show()
                 }
             }
 
     }
 
+    // Átirányítás a MenuActivity aktivitásra
     override fun callMenu() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
     }
-
-}
+    }
